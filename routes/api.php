8e3d92a1366ed373e276c53fcd25576f677300a4
Evan <evanslalo@gmail.com>
2024-01-11 06:48:52 -0500

    auth

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('games', GameController::class);
-Route::apiResource('polls', PollController::class);
+Route::middleware(['auth:sanctum', 'abilities:*'])->group(function() {
+    Route::apiResource('games', GameController::class);
+    Route::apiResource('polls', PollController::class);
+});
